eb449094ebbea2d69b24cbf78cfb383fa400f5dc
ptnaeiscute <116319308+ptnaeiscute@users.noreply.github.com>
2022-10-21 06:44:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Ngọc Ánh àaaaaa!',
+    introDesc: `Tuy rằng trái đất hình tròn
+    nhưng ta cách nhau 6.700km sao em lại đi
+    lạc vào tim anh thế kiaaa`,
+    btnIntro: 'Ấn zô đây iii',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'Em cũng thích anh <33',
+    btnNo: 'Anh ngủ đi rồi mơ nhé :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại va trúng vào anh <3',
+    btnReply: 'Ấn zô đây để gửi nee <3',
+    reply: 'Em cũng hông biết nữa chắc là do anh dễ thương giống emmm <33333333',
+    mess: 'Anh biết mò 🥰. Yêu em nhìu nhìu 😘😘',
+    messDesc: 'Tối nay anh bay về đón em đi chơi nhéeee.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
